5b339c3af0ca2a06c4eae2a4e9b6126c70b73e0e
cylcoder <cylcoder@gmail.com>
2025-06-27 01:31:22 +0900

    Section 3: 조회용 샘플 데이터 입력

---
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -0,0 +1,104 @@
+package jpabook.jpashop;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.DeliveryStatus;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+/*
+* 총 주문 2개
+* userA
+*   JPA1 BOOK
+*   JPA2 BOOK
+* userB
+*   SPRING1 BOOK
+*   SPRING2 BOOK
+* */
+
+//@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+  private final InitService initService;
+
+  @PostConstruct
+  public void init() {
+    initService.dbInit1();
+    initService.dbInit2();
+  }
+
+  @Component
+  @Transactional
+  @RequiredArgsConstructor
+  static class InitService {
+
+    private final EntityManager entityManager;
+
+    public void dbInit1() {
+      Member member = createMember("userA", "서울", "1", "1000");
+      entityManager.persist(member);
+
+      Book book1 = createBook("JPA1 BOOK", 10000, 100);
+      entityManager.persist(book1);
+
+      Book book2 = createBook("JPA2 BOOK", 210000, 100);
+      entityManager.persist(book2);
+
+      Delivery delivery = createDelivery(member);
+      OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+      OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+      Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+      entityManager.persist(order);
+    }
+
+    public void dbInit2() {
+      Member member = createMember("userB", "진주", "2", "2222");
+      entityManager.persist(member);
+
+      Book book1 = createBook("SPRING1 BOOK", 20000, 200);
+      entityManager.persist(book1);
+
+      Book book2 = createBook("SPRING2 BOOK", 40000, 300);
+      entityManager.persist(book2);
+
+      Delivery delivery = createDelivery(member);
+      OrderItem orderItem1 = OrderItem.createOrderItem(book1, 20000, 3);
+      OrderItem orderItem2 = OrderItem.createOrderItem(book2, 40000, 4);
+      Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+      entityManager.persist(order);
+    }
+
+    private Member createMember(String name, String city, String street, String zipcode) {
+      Member member = new Member();
+      member.setName(name);
+      Address address = new Address(city, street, zipcode);
+      member.setAddress(address);
+      return member;
+    }
+
+    private Book createBook(String name, int price, int stockQuantity) {
+      Book book = new Book();
+      book.setName(name);
+      book.setPrice(price);
+      book.setStockQuantity(stockQuantity);
+      return book;
+    }
+
+    private Delivery createDelivery(Member member) {
+      Delivery delivery = new Delivery();
+      delivery.setAddress(member.getAddress());
+      delivery.setStatus(DeliveryStatus.READY);
+      return delivery;
+    }
+
+  }
+
+}
