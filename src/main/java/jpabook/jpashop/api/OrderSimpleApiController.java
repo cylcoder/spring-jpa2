cdeb27d87100b081014622e1befc8a98d015b64b
cylcoder <cylcoder@gmail.com>
2025-06-27 11:03:24 +0900

    Section 4: 간단한 주문 조회 V2 - 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,7 +1,10 @@
 package jpabook.jpashop.api;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
@@ -33,6 +36,37 @@ public class OrderSimpleApiController {
           o.getDelivery().getAddress();
         });
     return orders;
+  }
+
+  /*
+  * V2. 엔티티 조회 후 DTO로 변환(fetch join X)
+  *   - 단점: 지연로딩으로 쿼리 N번 호출
+  * */
+  @GetMapping("/api/v2/simple-orders")
+  public List<SimpleOrderDto> orderV2() {
+    return orderRepository
+        .findAll()
+        .stream()
+        .map(SimpleOrderDto::from)
+        .toList();
+  }
+
+  public record SimpleOrderDto(
+      Long orderId,
+      String name,
+      LocalDateTime orderDate,
+      OrderStatus orderStatus,
+      Address address
+  ) {
+
+    public static SimpleOrderDto from(Order order) {
+      return new SimpleOrderDto(
+          order.getId(),
+          order.getMember().getName(),
+          order.getOrderDate(),
+          order.getStatus(),
+          order.getDelivery().getAddress());
+    }
   }
 
 }
